d9de65829a4c814de8e368ac3f1734e4fed81ee4
AydenBravender <aydenbravender@gmail.com>
2024-11-22 16:19:25 -0700

    aydens update

--- src/kipp_arm_moveit/launch/move_group.launch.py
+++ src/kipp_arm_moveit/launch/move_group.launch.py
@@ -3,10 +3,10 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("kipps_arm", package_name="kipp_moveit").to_moveit_configs()
     
+    moveit_config = MoveItConfigsBuilder("kipps_arm", package_name="kipp_arm_moveit").to_moveit_configs()
     # Add default move_group_capabilities if missing
     if "capabilities" not in moveit_config.move_group_capabilities:
         moveit_config.move_group_capabilities["capabilities"] = "move_group/MoveGroupCartesianPathService,move_group/MoveGroupExecuteService"
-
+        
     return generate_move_group_launch(moveit_config)
